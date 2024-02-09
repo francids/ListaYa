80f0d47da52cb45b6c2b70bd325aa51d07deb2a1
Francisco <drmalito.weid@gmail.com>
2024-02-08 22:16:41 -0400

    Se corrigió el error de las importaciones de las fuentes

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { loadAsync } from "expo-font";
@@ -6,51 +8,57 @@ import CardTask from "./components/CardTask";
 import Divider from "./components/Divider";
 import AddIconButton from "./components/AddIconButton";
 
+const tasks = [
+  {
+    id: 1,
+    title: "Tarea 1",
+    description: "Descripción de la tarea 1",
+    date: "2021-10-10",
+  },
+  {
+    id: 2,
+    title: "Tarea 2",
+    description: "Descripción de la tarea 2",
+    date: "2021-10-11",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Tarea 3",
+    description: "Descripción de la tarea 3",
+    date: "2021-10-12",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Tarea 4",
+    description: "Descripción de la tarea 4",
+    date: "2021-10-13",
+  },
+  {
+    id: 5,
+    title: "Tarea 5",
+    description: "Descripción de la tarea 5",
+    date: "2021-10-14",
+  },
+];
+
 export default function App() {
-  console.log("App started");
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-  const tasks = [
-    {
-      id: 1,
-      title: "Tarea 1",
-      description: "Descripción de la tarea 1",
-      date: "2021-10-10",
-    },
-    {
-      id: 2,
-      title: "Tarea 2",
-      description: "Descripción de la tarea 2",
-      date: "2021-10-11",
-      completed: true,
-    },
-    {
-      id: 3,
-      title: "Tarea 3",
-      description: "Descripción de la tarea 3",
-      date: "2021-10-12",
-      completed: true,
-    },
-    {
-      id: 4,
-      title: "Tarea 4",
-      description: "Descripción de la tarea 4",
-      date: "2021-10-13",
-    },
-    {
-      id: 5,
-      title: "Tarea 5",
-      description: "Descripción de la tarea 5",
-      date: "2021-10-14",
-    },
-  ];
+  useEffect(() => {
+    loadAsync({
+      "DMSans-Black": require("./assets/fonts/DMSans-Black.ttf"),
+      "DMSans-Bold": require("./assets/fonts/DMSans-Bold.ttf"),
+      "DMSans-Medium": require("./assets/fonts/DMSans-Medium.ttf"),
+      "DMSans-Regular": require("./assets/fonts/DMSans-Regular.ttf"),
+      "DMSans-Light": require("./assets/fonts/DMSans-Light.ttf"),
+    }).then(() => setFontLoaded(true));
+  }, []);
 
-  loadAsync({
-    "DMSans-Black": require("./assets/fonts/DMSans-Black.ttf"),
-    "DMSans-Bold": require("./assets/fonts/DMSans-Bold.ttf"),
-    "DMSans-Medium": require("./assets/fonts/DMSans-Medium.ttf"),
-    "DMSans-Regular": require("./assets/fonts/DMSans-Regular.ttf"),
-    "DMSans-Light": require("./assets/fonts/DMSans-Light.ttf"),
-  });
+  if (!fontLoaded) {
+    return <View />;
+  }
 
   return (
     <View style={styles.container}>
